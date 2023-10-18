fded52365a26470bccdfe26a62e011d2cfa45739
B3thy <b3thy.bap@gmail.com>
2023-10-18 13:47:15 -0300

    Update guessing_games.py
    * I've added a "guess the number" game!

--- guessing_games.py
+++ guessing_games.py
@@ -41,14 +41,48 @@ def rps():
     main()
 
 
+def gtn():
+
+    gtn_w = 0
+    gtn_l = 0
+    computer_number = random.randint(0, 10)
+    while True:
+        user_number = input(
+            "Choose your number! Between 0 and 10 or press q to exit.")
+        if str(user_number) == "q":
+            break
+        user_number = int(user_number)
+        if user_number >= 11 or user_number <= -1:
+            print("please enter a valid number")
+            break
+        if user_number == computer_number:
+            print("You won! you guessed the number correctly!")
+            gtn_w += 1
+            computer_number = random.randint(0, 10)
+        elif user_number < computer_number:
+            print("The number is higher!")
+            gtn_l += 1
+        elif user_number > computer_number:
+            print("The number is lower!")
+            gtn_l += 1
+    if (user_number == "q"):
+        print("Wins: " + str(gtn_w))
+        print("guesses: " + str(gtn_l))
+        main()
+    gtn()
+
+
 def main():
     print("Choose your game: ")
     print("1. Rock, Paper, Scissors")
-    print("2. quit")
+    print("2. Guess the number")
+    print("3. quit")
     user_input = input("Enter your choice: ")
     if user_input == str(1):
         rps()
     elif user_input == str(2):
+        gtn()
+    elif user_input == str(3):
         print("Cya! Until next time")
 
 
